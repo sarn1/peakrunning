8c58391dc01f5449bdababc459171aecbfd35453
Webvolution <spham@webvolutionchicago.com>
2016-09-14 19:59:09 -0500

    home

--- footer.php
+++ footer.php
@@ -1,9 +1,30 @@
 <?php wp_footer(); ?>
 
 <footer>
-    <?php dynamic_sidebar('Footer'); ?>
-    <p class="copyright">Copyright &copy;<?php echo date("Y"); ?> All rights reserved. <a rel="nofollow" href="http://www.webvolutionchicago.com" target="_blank">Designed by Webvolution</a></p>
+  <div class="row">
+    <div class="four columns left">
+      follow us
+      <ul class="footer_social_media">
+        <li><a href="https://www.facebook.com/pages/Peak-Running/616791905045685?ref=hl" target="_blank"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
+        <li><a href="#" target="_blank"><i class="fa fa-twitter-square" aria-hidden="true"></i></a></li>
+        <li><a href="#" target="_blank"><i class="fa fa-google-plus-square" aria-hidden="true"></i></a></li>
+        <li><a href="#" target="_blank"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+        <li><a href="https://www.pinterest.com/tiffanycruicksh/peak-running-decor/" target="_blank"><i class="fa fa-pinterest-square" aria-hidden="true"></i></a></li>
+      </ul>
+    </div>
+    <div class="eight columns right">
+      subscribe to the SneakPeak newsletter
+      <div id="mc_embed_signup">
+        <form action="http://peakrunningco.us8.list-manage.com/subscribe/post?u=17b448c5b686703a022c55143&amp;id=948fdbb09a" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank">
+          <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required="">
+          <div style="position: absolute; left: -5000px;"><input type="text" name="b_17b448c5b686703a022c55143_948fdbb09a" value=""></div>
+          <input type="submit" value="»" name="subscribe" id="mc-embedded-subscribe">
+        </form>
+      </div>
+    </div>
+  </div>
 </footer>
+<p class="copyright">&copy;<?php echo date("Y"); ?> <?php  echo get_bloginfo();  ?> All rights reserved. <a rel="nofollow" href="http://www.webvolutionchicago.com" target="_blank">Designed by Webvolution</a></p>
 
 <script type="text/javascript"><?php
 	$client = 'UA-50397420-1';
--- index.php
+++ index.php
@@ -1,20 +1,48 @@
 <?php get_header(); ?>
 <main role="main">
-  <?php
-  if (have_posts()) :
-    while (have_posts()) : the_post(); ?>
-      <article>
-        <h1><?php the_title(); ?></h1>
-        <?php the_content(); ?>
-      </article>
+<?php
+  if (have_posts()) {
+    while (have_posts()) {
+      the_post();
+
+      if (is_front_page()) {
+        //homepage slider
+        if ( function_exists( 'soliloquy' ) ) { soliloquy( '32' ); }
+?>
+        <!-- home middle -->
+        <div class="row home_middle">
+          <div class="four columns left">
+            <img src="/wp-content/uploads/2016/09/homepage_middle_left_image.jpeg" alt="Peak Running Merchandise" title="Peak Running Merchandise">
+          </div>
+          <div class="four columns mid">
+            <img src="/wp-content/uploads/2016/09/homepage_middle_middle_image.jpeg" alt="Our Upcoming Events" title="Our Upcoming Events">
+          </div>
+
+          <div class="four columns right">
+            <img src="/wp-content/uploads/2016/09/homepage_middle_right_image.jpeg" alt="Why Chose Up For Your Next Visit" title="Why Chose Up For Your Next Visit">
+          </div>
+        </div>
+
+<?php
+      } else {
+        ?>
+        <article>
+          <h1><?php the_title(); ?></h1>
+          <?php the_content(); ?>
+        </article>
+
+
+        <!--
       <aside>
         <?php get_sidebar(); ?>
       </aside>
-  <?php
-    endwhile;
-  elseif (is_404()) :
-      get_template_part('partials','404');
-  endif;
+-->
+<?php
+      }
+    }
+  } elseif (is_404()) {
+    get_template_part('partials', '404');
+  }
   ?>
 </main>
 <?php get_footer(); ?>
